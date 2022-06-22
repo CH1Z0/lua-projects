f110bc6f1058dff2265ddef3b3b82813a6c90aca
vTiiizi <107821300+vTiiizi@users.noreply.github.com>
2022-06-22 14:06:35 -0300

    Update Duolingo.lua

--- Events/Duolingo.lua
+++ Events/Duolingo.lua
@@ -1,6 +1,5 @@
 game:GetService("StarterGui"):SetCore("SendNotification", { 
     Title = "Well Umm This Is Awkward";
     Text = "Looks Like Luna Has Been Patched";
-    Icon = "http://www.roblox.com/asset/?id=00000000";
     Duration = 1213331231;
 })
