7f9ba01c64eed1a8cf9f8ea188a88297a0b2d9a6
tizi <107821300+arctisss@users.noreply.github.com>
2023-03-16 17:53:28 -0300

    Update Emoji.lua

--- Sources/Emoji.lua
+++ Sources/Emoji.lua
@@ -1,5 +1,5 @@
-
-local BlackHoleID = {
+-- \\ If the whitelist API is down make sure to refresh before adding a whitelist! // --
+local CometID = {
 	[4301887151] = true, --//Solar
     [4407833389] = true; --//Arctis
 }
@@ -7,17 +7,17 @@ local BlackHoleID = {
 local StarID = {}
 
 local MoonID = {
-	[3867097501] = true,
+	[3468728207] = true,
 }
 
 
 
 function premium()
 	for _,v in pairs(game:GetService('Players'):GetChildren()) do
-		if BlackHoleID[v.UserId] then
+		if CometID[v.UserId] then
 			if v.Character then
 				if v.Character.Parent.Name == 'Players' then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🌌]'..v.DisplayName)
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[☄]'..v.DisplayName)
 				end
 			end
 		elseif StarID[v.UserId] then
@@ -35,11 +35,11 @@ function premium()
 		elseif v.Character then
 			if v.Character.Parent.Name == 'Players' then
 				if not v.Character.UpperTorso:FindFirstChild('BodyBackAttachment') then
-					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[☄]'..v.DisplayName)
+					v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🌑]'..v.DisplayName)
 				end
 			end
 		end
 	end
 end
 local success,err = pcall(premium)
-return StarID
+return CometID
